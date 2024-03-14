eb5611fa870d5c8216edfbb2ebd2854820e3b872
Osa-nag00 <naghiseosa2000@gmail.com>
2024-03-14 18:58:50 -0400

    add function to pad image with zeros for 3x3 convolution

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@
 
 using namespace cv;
 
+/*
+ * Pad image it zeros on all borders
+ * This is so we can convolve with 3x3 kernal
+ */
+Mat fillWithZeros(Mat image)
+{
+    Mat padded;
+    int padding = 3;
+
+    padded.create(image.rows + 2 * padding, image.cols + 2 * padding, image.type());
+    padded.setTo(cv::Scalar::all(0));
+    image.copyTo(padded(Rect(padding, padding, image.cols, image.rows)));
+    return padded;
+}
+
 int main()
 {
     // sets the dir where we search for photos using "samples::findFile()"
@@ -15,12 +30,14 @@ int main()
     std::string inputImagePath = samples::findFile("sample.png");
     Mat inputImage = imread(inputImagePath, IMREAD_GRAYSCALE);
 
+    Mat padded = fillWithZeros(inputImage);
+
     if (inputImage.empty())
     {
         std::cout << "Image was not loaded properly" << std::endl;
     }
 
-    imshow("Display Window", inputImage);
+    imshow("Display Window", padded);
     waitKey(0); // wait for a keystroke in the window
 
     // TODO: get to the multithreading sobel part after exam
