83c62a26f1e0d3b2c0654a4bb190eed4a0649ca4
Osa-nag00 <naghiseosa2000@gmail.com>
2024-03-15 10:21:46 -0400

    add util class and function to write image pixel values to txt for debugging

--- main.cpp
+++ main.cpp
@@ -1,55 +1,12 @@
+#include "headers/localUtil.hpp"
 #include "headers/convolution.hpp"
-#include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/utility.hpp>
-// #include <string>
-// #include <fstream>
-// #include <iostream>
+#include <iostream>
+#include <fstream>
 
 using namespace cv;
-
-/**
- * Writes the pixel values of an image to a text file.
- *
- * @param image The input image.
- * @param filename The name of the text file to write the pixel values to.
- */
-// void writeImageToTxtFile(const cv::Mat &image, const std::string &filename)
-// {
-//     std::ofstream file("mame.txt");
-
-//     file << "wooords" << std::endl;
-
-//     if (file.is_open())
-//     {
-//         for (int row = 0; row < image.rows; ++row)
-//         {
-//             for (int col = 0; col < image.cols; ++col)
-//             {
-//                 file << static_cast<int>(image.at<uchar>(row, col)) << " ";
-//             }
-//             file << std::endl;
-//         }
-//         file.close();
-//         std::cout << "Image written to " << filename << std::endl;
-//     }
-//     else
-//     {
-//         std::cout << "Failed to open file for writing" << std::endl;
-//     }
-// }
-
-Mat fillWithZeros(Mat image)
-{
-    Mat padded;
-    int padding = 1;
-
-    padded.create(image.rows + 2 * padding, image.cols + 2 * padding, image.type());
-    padded.setTo(cv::Scalar::all(0));
-    image.copyTo(padded(Rect(padding, padding, image.cols, image.rows)));
-    return padded;
-}
 
 int main()
 {
@@ -64,11 +21,12 @@ int main()
         std::cout << "Image was not loaded properly" << std::endl;
     }
 
-    // Mat padded = fillWithZeros(inputImage);
-    // writeImageToTxtFile(padded, "padded");
-    // printImage(padded);
+    Mat padded = localUtil::fillWithZeros(inputImage);
 
-    imshow("Display Window", inputImage);
+    localUtil::writeImageToTxt(inputImage, "Regular");
+    localUtil::writeImageToTxt(padded, "padded");
+
+    imshow("Display Window", padded);
     waitKey(0); // wait for a keystroke in the window
 
     // TODO: get to the multithreading sobel part after exam
