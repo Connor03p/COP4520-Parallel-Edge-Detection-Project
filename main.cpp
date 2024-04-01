2e50793a47346508d29f1e84df574f07f7c4eea7
Osa-nag00 <naghiseosa2000@gmail.com>
2024-03-31 21:45:54 -0400

    added for loops for testing code

--- main.cpp
+++ main.cpp
@@ -12,36 +12,49 @@ using namespace cv;
 int main()
 {
 
-    int numThreads = 1;
-
     std::string filename = "sample3.png";
-    std::string path = "/src/output_imgs/" + filename;
-    Mat image = localUtil::loadImageFromFile(filename, cv::ImreadModes::IMREAD_GRAYSCALE);
-    MultithreadedSobel_V1 sobel(numThreads, 130);
-    Mat result;
-    int numTest = 25;
-    double avgTime = 0;
-    // TODO: this is used to time the function
+    std::vector<std::string> photos{"sample1.png", "sample2.png", "sample3.png"};
 
-    for (; numThreads <= 8;)
+    for (std::string filename : photos)
     {
-        for (int test = 0; test < numTest; test++)
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            result = sobel.performSobelEdgeDetection(image);
-            auto end = std::chrono::high_resolution_clock::now();
+        int numThreads = 1;
+        int threshold = 130;
+        std::string path = "/src/output_imgs/" + filename;
+        Mat image = localUtil::loadImageFromFile(filename, cv::ImreadModes::IMREAD_GRAYSCALE);
+        MultithreadedSobel_V2 sobel(numThreads, threshold);
+        Mat result;
+        int numTest = 15;
+        double avgTime = 0;
+        // TODO: this is used to time the function
 
-            std::chrono::duration<double> duration = end - start;
-            double executionTime = duration.count();
-            avgTime += executionTime;
+        for (; numThreads <= 8;)
+        {
+            for (int test = 0; test < numTest; test++)
+            {
+                auto start = std::chrono::high_resolution_clock::now();
+                result = sobel.performSobelEdgeDetection(image);
+                auto end = std::chrono::high_resolution_clock::now();
+
+                std::chrono::duration<double> duration = end - start;
+                double executionTime = duration.count();
+                avgTime += executionTime;
+            }
+
+            std::cout << "METHOD B" << std::endl;
+            std::cout << "Result size: " << result.size() << std::endl;
+            std::cout << "Number Of Test: " << numTest << std::endl;
+            std::cout << "Threshold value: " << threshold << std::endl;
+            std::cout << "Number Threads: " << numThreads << std::endl;
+            std::cout << "Average Time: " << (avgTime / numTest) << "s" << std::endl;
+            std::cout << "\n"
+                      << std::endl;
+
+            imwrite(path, result);
+            numThreads *= 2;
         }
 
-        std::cout << "Result size: " << result.size() << std::endl;
-        std::cout << "Number Threads: " << numThreads << std::endl;
-        std::cout << "Average Time: " << (avgTime / numTest) << "s" << std::endl;
-
-        imwrite(path, result);
-        numThreads *= 2;
+        std::cout << "\n"
+                  << std::endl;
     }
 
     // localUtil::writeImageToTxt(result, "mountain");
